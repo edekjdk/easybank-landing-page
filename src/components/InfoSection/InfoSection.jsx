d612c19b90d429171ee59d6b3a7e66b01e95c96d
Kacper <luszczkacper42@gmail.com>
2023-08-23 23:35:07 +0200

    added object whith data which is nescessary in InfoItem component

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,4 +1,31 @@
 import { InfoItem } from './InfoItem';
+import iconApi from '../../assets/images/iconApi.svg';
+import iconBudgeting from '../../assets/images/iconBudgeting.svg';
+import iconOnboarding from '../../assets/images/icononboarding.svg';
+import iconOnline from '../../assets/images/iconOnline.svg';
+
+const data = [
+  {
+    image: iconOnline,
+    title: 'Online Banking',
+    text: 'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.',
+  },
+  {
+    image: iconBudgeting,
+    title: 'Simple Budgeting',
+    text: 'See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.',
+  },
+  {
+    image: iconOnboarding,
+    title: 'Fast Onboarding',
+    text: '  We don’t do branches. Open your account in minutes online and start taking control of your finances right away.',
+  },
+  {
+    image: iconApi,
+    title: 'Open API',
+    text: 'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
+  },
+];
 
 export const InfoSection = () => {
   return (
