e580480c5e290d4e5cf8edd71b444a06c473da07
Kacper <luszczkacper42@gmail.com>
2023-08-27 10:56:10 +0200

    added structure into footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,47 @@
+import { StyledFooter } from './style/Footer.styled';
+import { Logo } from '../Utils/Logo';
+import { Button } from '../Utils/Button';
+import iconFacebook from '../../assets/images/iconFacebook.svg';
+import iconYoutube from '../../assets/images/iconYoutube.svg';
+import iconTwitter from '../../assets/images/iconTwitter.svg';
+import iconPinterest from '../../assets/images/iconPinterest.svg';
+import iconInstagram from '../../assets/images/iconInstagram.svg';
+
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <StyledFooter>
+      <section>
+        <Logo />
+        <ul>
+          <li>
+            <img src={iconFacebook} alt='fb' />
+          </li>
+          <li>
+            <img src={iconYoutube} alt='yt' />
+          </li>
+          <li>
+            <img src={iconTwitter} alt='twt' />
+          </li>
+          <li>
+            <img src={iconPinterest} alt='pin' />
+          </li>
+          <li>
+            <img src={iconInstagram} alt='ig' />
+          </li>
+        </ul>
+      </section>
+      <ul>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>Careers</li>
+        <li>Support</li>
+        <li>Privacy Policy</li>
+      </ul>
+      <section>
+        <Button />
+        <h5>&copy; Easybank. All Rights Reserved</h5>
+      </section>
+    </StyledFooter>
+  );
 };
