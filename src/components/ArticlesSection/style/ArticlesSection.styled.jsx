109f266b7d9839a5ee2a9a382abb4d952f62eb0b
Kacper <luszczkacper42@gmail.com>
2023-08-27 10:34:30 +0200

    small styling changes in articles section component

--- src/components/ArticlesSection/style/ArticlesSection.styled.jsx
+++ src/components/ArticlesSection/style/ArticlesSection.styled.jsx
@@ -1,14 +1,14 @@
 import { styled } from 'styled-components';
 
 export const StyledMainSection = styled.section`
-  padding: 0 1.4rem;
+  padding: 3rem 1.4rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
   @media screen and (min-width: 1440px) {
-    padding: 0 12rem;
+    padding: 3rem 12rem;
   }
 
   h2 {
@@ -16,7 +16,7 @@ export const StyledMainSection = styled.section`
     font-weight: 500;
     color: hsl(233, 26%, 24%);
     text-align: center;
-    margin: 3rem 0;
+    margin-bottom: 3rem;
   }
 `;
 
